6d1916b8b6ad96453fef34545c70a09823218764
Yuxiang Liu <yl26417p@pace.edu>
2022-03-31 21:14:39 -0400

    clickable

--- PlayTrade/app/src/main/java/com/example/jetpack/HomeActivity.java
+++ PlayTrade/app/src/main/java/com/example/jetpack/HomeActivity.java
@@ -2,13 +2,27 @@ package com.example.jetpack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void car_detailActivity(View view) {
+        Intent intent = new Intent(this, activity_car_detail.class);
+        startActivity(intent);
+    }
+
+    public void stack_detailActivity(View view) {
+        Intent intent = new Intent(this, activity_stack_detail.class);
+        startActivity(intent);
     }
 }
